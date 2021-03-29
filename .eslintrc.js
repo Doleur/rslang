2f50436f0f7845f4208588bd70a805b38a792238
Andrey Marhalyk <aom@anadeainc.com>
2021-03-30 00:09:18 +0300

    Add eslint plugin for sorting imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'simple-import-sort'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -24,6 +24,25 @@ module.exports = {
     }
   },
   rules: {
-    'linebreak-style': ['error', 'unix']
+    'linebreak-style': ['error', 'unix'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            '^(_http_agent|_http_client|_http_common|_http_incoming|_http_outgoing|_http_server|_stream_duplex|_stream_passthrough|_stream_readable|_stream_transform|_stream_wrap|_stream_writable|_tls_common|_tls_wrap|assert|async_hooks|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|http2|https|inspector|module|net|os|path|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|trace_events|tty|url|util|v8|vm|worker_threads|zlib)(/.*|$)'
+          ],
+          ['^react', '^styled-components', '^@?\\w', '^\\u0000'],
+          [
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./?$',
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$'
+          ],
+          ['^.+\\.s?css$']
+        ]
+      }
+    ]
   }
 };
