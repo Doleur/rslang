bd7b0abcaaf46db2f44623a0ce80ea22d0ad9378
Andrey Marhalyk <aom@anadeainc.com>
2021-03-30 20:30:50 +0300

    Fix app component after rebase

---
+++ src/components/App/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Footer from '../Footer';
+import GroupWordsPage from '../GroupWordsPage';
+import Header from '../Header';
+import MainPage from '../MainPage';
+import SignUp from '../SignUp';
+import Textbook from '../Textbook';
+
+const App = () => {
+  return (
+    <Container>
+      <Router>
+        <Header />
+        <Route path="/" render={() => <MainPage />} exact />
+        <Route path="/signup" render={() => <SignUp />} exact />
+        <Route
+          path="/textbook"
+          render={() => {
+            return <Textbook />;
+          }}
+          exact
+        />
+        <Route
+          path="/textbook/group/:id"
+          render={({ match }) => {
+            const { id } = match.params;
+            return <GroupWordsPage groupId={id} />;
+          }}
+        />
+        <Footer />
+      </Router>
+    </Container>
+  );
+};
+
+export default App;
