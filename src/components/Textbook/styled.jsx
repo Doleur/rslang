7fa640ccace3f8b10a45a32c3f33be458614c679
Doleur <voleur16@gmail.com>
2021-03-31 19:44:59 +0300

    add video

--- src/components/Textbook/styled.jsx
+++ src/components/Textbook/styled.jsx
@@ -2,14 +2,7 @@ import styled from 'styled-components';
 
 export const TextbookWrapper = styled.div`
   display: flex;
-  background-color: blue;
+  flex-wrap: wrap;
+  justify-content: space-around;
   height: 80vh;
-`;
-
-export const GroupWordsBtn = styled.div`
-  font-size: 20px;
-  background-color: pink;
-  height: 50px;
-  margin: 0 10px;
-  padding: 10px
 `;
---
+++ src/components/VideoYoutube/VideoYoutube.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import {string} from 'prop-types';
+import './video-yuotube.scss';
+
+const VideoYouTube = ({videoId}) => {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 0
+    }
+  };
+  const onReady = (event) => {
+    event.target.pauseVideo();
+  };
+  return <YouTube videoId={videoId} opts={opts} onReady={onReady} />;
+};
+
+VideoYouTube.propTypes = {
+  videoId: string
+};
+
+export default VideoYouTube;
