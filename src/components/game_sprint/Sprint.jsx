2de3a1b7a79d0c092a8e94deed529d751c6551e8
Doleur <voleur16@gmail.com>
2021-04-06 18:36:02 +0300

    fix start game

--- src/components/game_sprint/Sprint.jsx
+++ src/components/game_sprint/Sprint.jsx
@@ -6,7 +6,7 @@ import StartScreen from './StartScreen/StartScreen';
 import * as S from './styled';
 
 const Sprint = () => {
-  const [difficultiesWords, updateDifficultiesWords] = useState([1, 1]);
+  const [difficultiesWords, updateDifficultiesWords] = useState(0);
   const [isGameOver, updateIsGameOver] = useState(false);
 
   const gameScreen = () => {
