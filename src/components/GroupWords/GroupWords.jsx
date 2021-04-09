56aa9f756c6ddb8b17f9e8c9e2620f3127b30dd5
Andrey Marhalyk <aom@anadeainc.com>
2021-04-09 22:01:09 +0300

    Update styles for groups

--- src/components/GroupWords/GroupWords.jsx
+++ src/components/GroupWords/GroupWords.jsx
@@ -1,38 +1,81 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { useAuthentication } from '../../contexts/AuthenticationContext';
 import * as S from './styled';
 
 const GroupWords = ({ history, groupId, name }) => {
-  return (
-    <S.GroupWords>
-      <S.Text>{name}</S.Text>
-      <Button
+  const { currentUser } = useAuthentication();
+
+  const handleClick = (path) => {
+    if (!currentUser) return;
+
+    history.push(path);
+  };
+
+  const DifficultWordsButton = () => (
+    <span className="d-inline-block mr-auto ml-auto">
+      <S.NotebookButton
+        disabledbutton={currentUser ? null : 'true'}
         variant="outline-primary"
-        onClick={() => {
-          history.push(`/textbook/group/${groupId}`);
-        }}
-      >
-        Общее
-      </Button>
-      <Button
-        variant="outline-primary"
-        onClick={() => {
-          history.push(`/textbook/group/${groupId}/difficult_words`);
-        }}
+        onClick={() =>
+          handleClick(`/textbook/group/${groupId}/difficult_words`)
+        }
       >
         Сложные
-      </Button>
-      <Button
+      </S.NotebookButton>
+    </span>
+  );
+  const DeletedWordsButton = () => (
+    <span className="d-inline-block mr-auto ml-auto">
+      <S.NotebookButton
+        disabledbutton={currentUser ? null : 'true'}
         variant="outline-primary"
-        onClick={() => {
-          history.push(`/textbook/group/${groupId}/deleted_words`);
-        }}
+        onClick={() => handleClick(`/textbook/group/${groupId}/deleted_words`)}
       >
         Удаленные
-      </Button>
+      </S.NotebookButton>
+    </span>
+  );
+
+  return (
+    <S.GroupWords isUserLoggedIn={!!currentUser}>
+      <S.Text>{name}</S.Text>
+      <S.NotebookButton
+        variant="outline-primary"
+        onClick={() => handleClick(`/textbook/group/${groupId}`)}
+      >
+        Общее
+      </S.NotebookButton>
+      {currentUser ? (
+        <>
+          {DifficultWordsButton()}
+          {DeletedWordsButton()}
+        </>
+      ) : (
+        <>
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">
+                Войдите в систему чтобы получить доступ к этой секции.
+              </Tooltip>
+            }
+          >
+            {DifficultWordsButton()}
+          </OverlayTrigger>
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">
+                Войдите в систему чтобы получить доступ к этой секции.
+              </Tooltip>
+            }
+          >
+            {DeletedWordsButton()}
+          </OverlayTrigger>
+        </>
+      )}
     </S.GroupWords>
   );
 };
--- src/components/GroupWords/styled.jsx
+++ src/components/GroupWords/styled.jsx
@@ -1,8 +1,10 @@
+import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
 export const GroupWords = styled.div`
   align-content: center;
   background-color: #fff;
+  border-radius: 10px;
   box-shadow: 3px 2px 10px 1px rgba(34, 60, 80, 0.6);
   display: flex;
   flex-direction: column;
@@ -12,22 +14,38 @@ export const GroupWords = styled.div`
   margin: 50px 10px 10px;
   padding: 10px;
   width: 200px;
-  border-radius: 10px;
 
   &:hover {
-    cursor: pointer;
     transform: scale(1.05);
+  }
+
+  span:last-child {
+    margin-bottom: 25px;
   }
 
   button {
     margin: 0 auto;
     width: 130px;
     font-weight: bold;
-
-    &:last-child {
-      margin-bottom: 20px;
-    }
   }
+`;
+
+export const NotebookButton = styled(Button)`
+  ${({ disabledbutton }) =>
+    disabledbutton === 'true' &&
+    `
+    color: #007bff;
+    background-color: transparent;
+    border-color: #007bff;
+    opacity: .65;
+    cursor: not-allowed !important;
+
+    :hover {
+      color: #007bff;
+      background-color: transparent;
+      border-color: #007bff;
+    }
+  `}
 `;
 
 export const Text = styled.div`
