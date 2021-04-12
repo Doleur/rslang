fc9003d76cc6c94fc841a90f3f95b00cbe8f26d0
Doleur <voleur16@gmail.com>
2021-04-12 20:57:17 +0300

    fix style start screen sprint game

--- src/components/game_sprint/StartScreen/styled.jsx
+++ src/components/game_sprint/StartScreen/styled.jsx
@@ -1,23 +1,34 @@
 import styled from 'styled-components';
 
 export const StartScreen = styled.div`
+  text-align: center;
+  background-color: rgba(47, 61, 255, 0.15);
+  border-radius: 20px;
+  padding: 10px;
   width: 600px;
   height: 600px;
-  background-color: purple;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const CategoriesWrapper = styled.div`
   display: flex;
-  background-color: blue;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 70%;
 `;
 
 export const CategoryElement = styled.div`
-  font-size: 20px;
+  font-size: 30px;
   text-align: center;
-  min-width: 30px;
-  margin: 10px;
-  background-color: red;
+  min-width: 40px;
+  margin: 60px 10px 10px;
+  background-color: rgba(47, 102, 255, 0.527);
+  border-radius: 10px;
   &:hover {
     cursor: pointer;
+    transform: scale(1.05);
   }
 `;
